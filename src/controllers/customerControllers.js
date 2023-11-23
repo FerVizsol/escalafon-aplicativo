22404a475b49870b788bd00e2db7126031884f49
FerVizsol <fernando.vizcarra@ucsm.edu.pe>
2023-11-23 00:04:49 -0500

    asd

---
+++ src/controllers/customerControllers.js
@@ -0,0 +1,96 @@
+const controller = {};
+
+//selects
+controller.inicio = (req,res)=> {
+    req.getConnection((err,conn) =>{
+        conn.query("CALL listar_docentes()",(err,lista) =>{
+            if(err){
+                res.json(err);
+            }
+            console.log(lista);
+            res.render('inicio',{
+                data:lista,
+                busqueda : ""
+            });
+        })
+    });
+}
+
+controller.busqueda = (req,res) =>{
+    const busqueda = req.query.busqueda;
+    req.getConnection((err,conn) =>{
+        
+        if([busqueda] == undefined){
+            req.getConnection((err,conn) =>{
+                conn.query("CALL listar_docentes()",(err,lista) =>{
+                    if(err){
+                        res.json(err);
+                    }
+                    res.render('inicio',{
+                        data:lista,
+                        busqueda : ""
+                    });
+                })
+            });
+        }
+        conn.query("CALL buscar_docentes(?)",[busqueda],(err,lista) =>{
+            if (err){
+                res.render('inicio',{
+                    data : [[{
+                        numdoc : '',
+                        nom : '',
+                        cmodular : '',
+                        claboral : '',
+                        slaboral : ''
+                    }
+                ],{}],
+                busqueda
+                })
+            }
+            res.render('inicio',{
+                data:lista,
+                busqueda
+            })
+        });
+    })
+}
+
+controller.verlegajo = (req,res) =>{
+    const {id} = req.params;
+    res.render('legajo',{
+        
+    })
+}
+controller.anadirdocente = (req,res) => {
+    const {id} = req.params;
+    res.render('anadir',{
+
+    })
+}
+controller.editardocente = (req,res) => {
+    const {id} = req.params;
+    res.render('editar',{
+
+    })
+}
+controller.eliminardocente = (req,res) => {
+    const {id} = req.params;
+    res.render('eliminar',{
+        id : id
+    })
+}
+
+controller.delete = (req,res) => {
+    const {id} = req.params;
+    console.log({id})
+    req.getConnection((err,conn) =>{
+        conn.query("CALL eliminar_docente(?)",[id], (err,data) =>{
+            if (err){
+                console.log(err.message)
+            }else{
+                res.render('eliminado')
+            }
+        })
+    })
+}
+module.exports = controller;
---
+++ src/routes/customer.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const customerControllers = require('../controllers/customerControllers');
+const controller = require('../controllers/customerControllers');
+
+
+router.get('/', customerControllers.inicio);
+router.get('/busqueda', customerControllers.busqueda)
+router.get('/legajo/:id', customerControllers.verlegajo)
+router.get('/anadir', customerControllers.anadirdocente)
+router.get('/editar/:id', customerControllers.editardocente)
+router.get('/eliminar/:id', customerControllers.eliminardocente)
+router.get('/delete/:id', customerControllers.delete)
+module.exports = router;
